cee4a55726fd908b1562e9f8d08810707cf02f25
onmax <maximogarciamtnez@gmail.com>
2025-02-20 17:00:57 +0100

    chore: added tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,19 +1,18 @@
-import { createGenerator } from 'unocss'
+import { createGenerator, presetWind3 } from 'unocss'
 import { expect, it } from 'vitest'
-import { presetStarter } from '../src'
+import { presetScalePx } from '../src'
 
-it('presetStarter', async () => {
-  const uno = createGenerator({
-    presets: [presetStarter()],
+it('presetScalePx', async () => {
+  const uno = await createGenerator({
+    presets: [presetWind3(), presetScalePx()],
   })
   const presets = uno.config.presets
-  expect(presets).toHaveLength(1)
+  expect(presets).toHaveLength(2)
 
-  const { css } = await uno.generate('col-1 @active:col-2')
+  const { css } = await uno.generate('text-4', { preflights: false })
 
   expect(css).toMatchInlineSnapshot(`
     "/* layer: default */
-    .\\@active\\:col-2.active{width:calc(2 / 12 * 100%);}
-    .col-1{width:calc(1 / 12 * 100%);}"
+    .text-4{font-size:0.25rem;}"
   `)
 })
